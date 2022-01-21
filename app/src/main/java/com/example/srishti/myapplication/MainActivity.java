2fabefc8348902bd88bf7f0997056fe086a2c775
unknown <msgaur1997@gmail.com>
2022-01-21 15:26:55 +0530

    Android application Linkora

---
+++ app/src/main/java/com/example/srishti/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.srishti.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) {
+    }
+
+}
